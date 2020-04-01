6a5a97fd6ee50c0fd1a2ed643dce332546b97cdb
Santhosh Kumar <santhosh31195@gmail.com>
2020-04-01 15:58:29 -0700

    UI addtion

--- App.js
+++ App.js
@@ -1,49 +1,94 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.eventhead}>
-        <Text>Flix Bus</Text>
+    <View style={styles.page}>
+      <View style={styles.header}>
+        <Text>Sort by</Text>
+        <Button title = "Name"/>
+        <Button title = "Expiry" />
+        <Button title = "Add Event" />
       </View>
-      <View style={styles.eventbody}>
-        <Text>Coupon WQEUIRAO</Text>
-        <Text>Expiry 30 March 2021</Text>
-        <Text>Remind on 27 Nov 2020</Text>
+      <View style={styles.event}>
+        <View style={styles.eventhead}>
+          <Text style={styles.eventheadtext}>Flix Bus</Text>
+        </View>
+        <View style={styles.eventbody}>
+          <Text>Coupon WQEUIRAO</Text>
+          <Text>Expiry 30 March 2021</Text>
+          <Text>Remind on 27 Nov 2020</Text>
+        </View>
       </View>
-      <View style={styles.eventhead}>
-        <Text>Flix Bus1</Text>
+      <View style={styles.event}>
+        <View style={styles.eventhead}>
+          <Text style={styles.eventheadtext}>Flix Bus1</Text>
+        </View>
+        <View style={styles.eventbody}>
+          <Text>March 30 2021</Text>
+        </View>
       </View>
-      <View style={styles.eventbody}>
-        <Text>March 30 2021</Text>
+      <View style={styles.event}>
+        <View style={styles.eventhead}>
+          <Text style={styles.eventheadtext}>Flix Bus1</Text>
+        </View>
+        <View style={styles.eventbody}>
+          <Text>March 30 2021</Text>
+        </View>
       </View>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.event}>
+        <View style={styles.eventhead}>
+          <Text style={styles.eventheadtext}>Flix Bus1</Text>
+        </View>
+        <View style={styles.eventbody}>
+          <Text>March 30 2021</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
     flex: 1,
-    backgroundColor: '#0ff',
+    flexDirection: 'column',
+    justifyContent: "space-between",
+    paddingTop: '10%',
+    paddingLeft: '5%',
+    paddingBottom: '5%',
+    paddingRight: '5%',
+    alignItems: "stretch",
+    justifyContent: "space-between",
+    
+  },
+  header:{
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'steelblue',
+    justifyContent: "space-between",
+    maxHeight: '10%',
+    padding: 10,
+  },
+  event: {
+    flex: 1,
+    flexDirection: 'column',
+    maxHeight: '20%',
   },
   eventhead:{
-    flex: 2,
-    backgroundColor: '#bbc7be',
-    fontSize: 30,
-    marginTop: 40,
-    marginLeft: 20,
-    marginRight: 10,
+    backgroundColor: 'skyblue',
+    padding: 10,
+    borderColor: 'white',
+    borderRadius: 10,
+    alignItems: "center"
+  },
+  eventheadtext:{
+    fontSize: 20,
     fontWeight: "bold",
   },
   eventbody:{
-    flex: 1,
-    backgroundColor: '#fff',
-    marginTop: 5,
-    marginLeft: 20,
-    marginRight: 10,
-    fontSize: 5,
-    maxHeight: 40,
+    backgroundColor: 'powderblue',
+    borderColor: 'white',
+    borderRadius: 10,
+    padding: 10,
   },
 });
